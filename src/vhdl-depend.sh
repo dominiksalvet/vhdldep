22a52e74d9ba5a03119879b1fc885760e088470f
Dominik Salvet <dominik.salvet@gmail.com>
2018-05-05 17:39:43 +0200

    Update vhdl-depend.sh
    The code has been cleaned and now supports new lines inside use
    sections.

--- src/vhdl-depend.sh
+++ src/vhdl-depend.sh
@@ -9,27 +9,50 @@
 # $2 - target name
 # $3 - object files prefix
 
-phase1='
-/^\s*use/!d
+remove_comments='s/--.*$//'
+
+get_uses='
+/^\s*use.*\;/b
+/^\s*use/,/\;/!d
+'
+
+one_use_per_line='
+/use/!b
+:next
+/\;/bdone
+N
+bnext
+:done
+s/\n//g
+'
+
+one_local_use_dependency_per_line='
 s/use//
 s/\s*//g
 /^ieee\|^std/d
 s/\;$//
 s/\.all$//
 s/^.*\.//
-s/^.*/&.o/
+s/^.*/&\.o/
 s|^|'"$3"'|
 '
-phase2='
+
+one_local_dependency_per_line='1s/^/'"$1"'\n/'
+
+make_rule_format='
 :next
 $bdone
 N
 bnext
 :done
 s/\n/ /g
-s|^|'"$1"' |
 s|^|'"$2"': |
 '
 
-sed -e "$phase1" "$1" |
-sed -e "$phase2"
+sed -e "$remove_comments" \
+    -e "$get_uses" \
+    "$1" | \
+sed -e "$one_use_per_line" \
+    -e "$one_local_use_dependency_per_line" | \
+sed -e "$one_local_dependency_per_line" \
+    -e "$make_rule_format"
