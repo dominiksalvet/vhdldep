74e930a5e3ba15586ec90b393282bcf1a1dec600
Dominik Salvet <dominik.salvet@gmail.com>
2018-05-05 02:26:44 +0200

    Update vhdl-depend.sh
    The vhdl-depend now work much more like gcc with -MMD flag - the file
    itself is now being added to a generated list of dependencies.
    
    Also the parameter order has been changed:
    $1 - VHDL file path
    $2 - target name
    $3 - object files prefix

--- src/vhdl-depend.sh
+++ src/vhdl-depend.sh
@@ -5,10 +5,31 @@
 # SPDX-License-Identifier: MIT
 #-------------------------------------------------------------------------------
 
-target=$(echo "$1" | sed -e 's|.*/||;s/\.[^\.]*$/.o/;s|^|'"$2"'|;')
+# $1 - VHDL file
+# $2 - target name
+# $3 - object files prefix
 
-command1='/^\s*use/!d;s/use//;s/\s*//g;/^ieee\|^std/d;s/\;$//;s/\.all$//;s/^.*\.//;s/^.*/&.o/;s|^|'"$2"'|;'
-command2=':next;$bdone;N;bnext;:done;s/\n/ /g;s|^|'"$target"': |;'
+phase1='
+/^\s*use/!d
+s/use//
+s/\s*//g
+/^ieee\|^std/d
+s/\;$//
+s/\.all$//
+s/^.*\.//
+s/^.*/&.o/
+s|^|'"$3"'|
+'
+phase2='
+:next
+$bdone
+N
+bnext
+:done
+s/\n/ /g
+s|^|'"$1"' |
+s|^|'"$2"': |
+'
 
-sed -e "$command1" "$1" |
-sed -e "$command2"
+sed -e "$phase1" "$1" |
+sed -e "$phase2"
